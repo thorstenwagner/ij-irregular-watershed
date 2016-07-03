fda7154be8f8a20cb9ffeadd2f0d31557c781952
jumpfunky <thotho@posteo.de>
2016-07-03 10:18:51 +0200

    Irregular watershed

--- src/main/java/de/biomedical_imaging/ij/irregular_watershed/Watershed_Irregular_Features.java
+++ src/main/java/de/biomedical_imaging/ij/irregular_watershed/Watershed_Irregular_Features.java
@@ -1,13 +1,24 @@
+package de.biomedical_imaging.ij.irregular_watershed;
+
+
 import ij.*;
 import ij.process.*;
+import ij.blob.Blob;
+import ij.blob.ManyBlobs;
 import ij.gui.*;
+
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
 import ij.plugin.filter.*;
 import ij.measure.Measurements;
 import ij.measure.ResultsTable;
 import ij.plugin.frame.PlugInFrame;
 import ij.gui.DialogListener;
 import ij.gui.GenericDialog;
+import ij.gui.ImageCanvas;
 import ij.plugin.filter.EDM;
 import ij.plugin.ImageCalculator;
 import ij.plugin.filter.ParticleAnalyzer;
@@ -44,6 +55,9 @@ import ij.plugin.filter.ParticleAnalyzer;
  * obtained by usage of this plugin or a modified version of it
  * 
  * Thank you
+ * 
+ * May 04, 2015: Bug fix: Now correctly works with stacks (Thorsten Wagner, wagner@biomedical-imaging.de)
+ * May 21, 2015: Now uses Jan's EDM based erosion and adds convexity based watershed which is scale invariant 
  *
  */
 
@@ -51,132 +65,163 @@ import ij.plugin.filter.ParticleAnalyzer;
 public class Watershed_Irregular_Features implements ExtendedPlugInFilter, DialogListener {
 	ImagePlus imp;
 	private double erosions = 1;
+	private double convexityThreshold = 0;
 	private PlugInFilterRunner pfr;
 	private int nPasses = 1;
 	private int pass;
 	private int flags = DOES_8G|KEEP_PREVIEW|SNAPSHOT;
-	
-
 	public int setup(String arg, ImagePlus imp) {
-		this.imp = imp;
+		this.imp = imp; 
 		if(!imp.getProcessor().isBinary()) {
 			IJ.error("works only on 8-bit binary images");
 			return DONE;
 		} else {
-			return DOES_8G;
+			return DOES_8G | DOES_STACKS;
 		}
 		
+	
 	}
-
 	public int showDialog(ImagePlus imp, String command, PlugInFilterRunner pfr) {
 		GenericDialog gd = new GenericDialog("Watershed Irregular Features");
-			gd.addNumericField("erosion cycle number:", erosions, 1, 5, "");
-	                gd.addPreviewCheckbox(pfr);	// passing pfr makes the filter ready for preview
-	                gd.addDialogListener(this);	// the DialogItemChanged method will be called on user input
-	                gd.addHelp("http://fiji.sc/BioVoxxel_Toolbox#Watershed_Irregular_Structures");
-	                gd.showDialog();		// display the dialog; preview runs in the background now
-	                if (gd.wasCanceled()) {
-	                	return DONE;
-	                }
-	               	IJ.register(this.getClass());	// protect static class variables (filter parameters) from garbage collection
-	        this.pfr = pfr;
-	        return IJ.setupDialog(imp, flags); // ask whether to process all slices of stack (if a stack)
+		gd.addNumericField("erosion cycle number:", erosions, 0, 5, "");
+		gd.addNumericField("Convexity_Threshsold", 0, 2);
+		gd.addPreviewCheckbox(pfr); // passing pfr makes the filter ready for preview
+		gd.addDialogListener(this); // the DialogItemChanged method will be called on user input
+		gd.addHelp("http://fiji.sc/BioVoxxel_Toolbox#Watershed_Irregular_Structures");
+		gd.showDialog(); // display the dialog; preview runs in the background now
+		if (gd.wasCanceled()) {
+			return DONE;
+		}
+		IJ.register(this.getClass()); // protect static class variables (filter parameters) from garbage collection
+		this.pfr = pfr;
+		return IJ.setupDialog(imp, flags); // ask whether to process all slices of stack (if a stack)
 	}
-
 	public boolean dialogItemChanged(GenericDialog gd, AWTEvent e) {
 		erosions = (int) gd.getNextNumber();
+		convexityThreshold = gd.getNextNumber();
 		if (gd.invalidNumber() || erosions<1) {
-			IJ.error("invalid number");
+			Checkbox previewCheckbox = (Checkbox) gd.getCheckboxes().firstElement();
+			if (previewCheckbox.getState()) {
+				previewCheckbox.setSize(130, 20);
+				previewCheckbox.setLabel("Invalid number");
+			}
 			return false;
 		} else {
 			return true;
 		}
 	}
-
 	public void run(ImageProcessor ip) {
-		ip.snapshot();
-		//boolean blackBackground = Prefs.blackBackground;
-		
+		// ip.snapshot();
+		Prefs.blackBackground = true;
 		boolean invertedLut = ip.isInvertedLut();
 		if(invertedLut) {
 			ip.invertLut();
 		}
-
-		ImagePlus erosionImp = imp.duplicate();
-		ImageProcessor erosionIP = erosionImp.getProcessor();
-
-		ImagePlus watershedImp = erosionImp.duplicate();
-		ImageProcessor watershedIP = watershedImp.getProcessor();
-
-		//dilate objects according to erosion cycle number entered by user
-		for(int n=0; n<=erosions; n++) {
-			erosionIP.dilate();
+		ImagePlus origImp = new ImagePlus("",ip.duplicate());
+		ImageProcessor erosionIP = ip.duplicate();
+		ImagePlus erosionImp = new ImagePlus("", erosionIP);
+		ImageProcessor watershedIP = ip.duplicate();
+		ImagePlus watershedImp = new ImagePlus("", watershedIP);
+		
+		
+		EDM edm = new EDM();
+		
+		
+		if(convexityThreshold==0){
+			//If the convexity threshold is set to 0 simple erode the image n times.
+			edm.toEDM(erosionIP);
+			erosionIP.threshold((int)erosions);
 		}
+		else{
+			//Do a seperate erosion for each connectec component (CC). If
+			//a CC has a convexity larger than the convexity threshold stop the erosion
+			//for this object
+			ArrayList<Blob> objects = new ArrayList<Blob>();
+			
+			
+			erosionImp = new ImagePlus("", erosionIP);
+			ManyBlobs mb = new ManyBlobs(erosionImp);
+			mb.setBackground(0);
+			mb.findConnectedComponents();
+			while(mb.size()>0) {
+				for (Blob blob : mb) {
+					if(blob.getConvexity() >convexityThreshold){
+						objects.add(blob);
+						erosionIP.setColor(Color.black);
+						
+						erosionIP.fillPolygon(blob.getOuterContour());
+					}
+				}
+				edm.toEDM(erosionIP);
+				erosionIP.threshold(2);
+				
+				erosionImp = new ImagePlus("", erosionIP);
+	
+				mb = new ManyBlobs(erosionImp);
+				mb.setBackground(0);
+				mb.findConnectedComponents();
 
+				
+			}
+			erosionIP.set(0);
+			for (Blob blob : objects) {
+				blob.setDefaultColor(Color.white);
+				blob.draw(erosionIP);
+			}
+		}
+		
 		//separate original objects with the normal watershed algorithm from IJ
-		EDM watershedEDM = new EDM();
-		watershedEDM.toWatershed(watershedIP);
-
+		
+		edm.toWatershed(watershedIP);
+		
+		
 		//first, the watershed separation lines are extracted
-		//then, the second image calculation keeps only those separators 
+		//then, the second image calculation keeps only those separators
 		//which overlap with an eroded particle
 		ImageCalculator calculateImages = new ImageCalculator();
-		ImagePlus extractedSeparatorsImp = calculateImages.run("XOR create", watershedImp, imp);	
-		
+		ImagePlus extractedSeparatorsImp = calculateImages.run("XOR create", watershedImp, origImp);
 		ImagePlus remainingSeparatorsImp = calculateImages.run("AND create", extractedSeparatorsImp, erosionImp);
-
+		
 		//the remaining separator lines are analyzed to get their starting position
 		//for later selection
-		int options = ParticleAnalyzer.CLEAR_WORKSHEET|ParticleAnalyzer.RECORD_STARTS;
+		int options = ParticleAnalyzer.RECORD_STARTS;
 		int measurements = Measurements.CENTROID;
-		ResultsTable rt = new ResultsTable();
-		ParticleAnalyzer pa = new ParticleAnalyzer(options, measurements, rt, 0.0, 999999999.9);
-
+		ResultsTable resultsTable = new ResultsTable();
+		ParticleAnalyzer pa = new ParticleAnalyzer(options, measurements, resultsTable, 0.0, 999999999.9);
 		pa.analyze(remainingSeparatorsImp);
 		int xStart, yStart;
-
 		watershedIP.setValue(255.0);
 		
 		//the remaining separation lines in their original size and orientation
 		//are copied into the watersheded image to close undesired separations
-		for(int r=0; r<rt.getCounter(); r++) {
-			xStart = (int) rt.getValue("XStart", r);
-			yStart = (int) rt.getValue("YStart", r);
+		for(int r=0; r<resultsTable.getCounter(); r++) {
+			xStart = (int)resultsTable.getValue("XStart", r);
+			yStart = (int)resultsTable.getValue("YStart", r);
 			IJ.doWand(extractedSeparatorsImp, xStart, yStart, 0.0, "8-connected");
 			Roi selectedSeparator = extractedSeparatorsImp.getRoi();
 			watershedIP.fill(selectedSeparator);
-			
 		}
-
+		//watershedImp.show();
 		//the corrected watersheded image is copied into the original to be able to
 		//undo the watershed if undesired
-		calculateImages.run("Copy", imp, watershedImp);
+		//origImp.getProcessor().setPixels(watershedImp.getProcessor().getPixels());
 		
 		if(invertedLut) {
 			ip.invertLut();
 		}
-		
-		imp.updateAndDraw();
-				
-		//prepare all unused ImagePlus, ImageProcessor and Constructors for carbage collection
-		watershedImp.close();
-		watershedEDM = null;
-		erosionImp.close();
-		extractedSeparatorsImp.close();
-		remainingSeparatorsImp.close();
-		rt = null;
-		pa = null;
+		for(int i = 0; i < ip.getWidth(); i++){
+			for(int j =0; j < ip.getHeight(); j++){
+				ip.putPixel(i, j, watershedImp.getProcessor().getPixel(i, j));
+			}
+		}
+
 	}
-
-
 	public void setNPasses (int nPasses) {
 		this.nPasses = nPasses;
 		pass = 0;
 	}
-
 	void showProgress(double percent) {
 		percent = (double)(pass-1)/nPasses + percent/nPasses;
 		IJ.showProgress(percent);
 	}
-
 }
